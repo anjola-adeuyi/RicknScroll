9255fdd3b38e337af11335c9d3e7b304ab7f682a
Anjola Adeuyi <57623705+anjola-adeuyi@users.noreply.github.com>
2024-04-19 15:22:58 +0100

    feat: implement filtering and custom search

--- src/components/SearchBar.tsx
+++ src/components/SearchBar.tsx
@@ -1,7 +1,28 @@
-import React from 'react';
+import React, { useState } from 'react';
 
-const SearchBar: React.FC = () => {
-  return <div>SearchBar</div>;
+interface SearchBarProps {
+  onSearch: (query: string) => void;
+}
+
+const SearchBar: React.FC<SearchBarProps> = ({ onSearch }) => {
+  const [query, setQuery] = useState<string>('');
+
+  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setQuery(e.target.value);
+    onSearch(e.target.value);
+  };
+
+  return (
+    <div className="my-4">
+      <input
+        type="text"
+        className="w-full text-black px-4 py-2 border rounded-md focus:outline-none focus:border-slate-500"
+        placeholder="Search characters by name..."
+        value={query}
+        onChange={handleChange}
+      />
+    </div>
+  );
 };
 
 export default SearchBar;
--- src/pages/Homepage.tsx
+++ src/pages/Homepage.tsx
@@ -1,18 +1,27 @@
-import React from 'react';
+import React, { useState } from 'react';
 import CharacterList from '../components/CharacterList';
 import useQuery from '../hooks/useQuery';
 import { CharacterResult } from '../types/types';
 import SearchBar from '../components/SearchBar';
 
 const Homepage: React.FC = () => {
+  const [searchQuery, setSearchQuery] = useState<string>('');
   const { data, loading, error } = useQuery<CharacterResult>('https://rickandmortyapi.com/api/character');
 
   // eslint-disable-next-line no-console
   console.log({ data, loading, error });
 
-  if (loading && !data?.results) return <div>Loading...</div>;
-  if (error) return <div>Error: {error}</div>;
-  if (!data) return <div>No data</div>;
+  const handleSearch = (query: string) => {
+    setSearchQuery(query);
+  };
+
+  const filteredCharacters = data?.results.filter((character) =>
+    character.name.toLowerCase().includes(searchQuery.toLowerCase())
+  );
+
+  if (loading && !data?.results) return <div className="text-white">Loading...</div>;
+  if (error) return <div className="text-white">Error: {error}</div>;
+  if (!filteredCharacters) return <div className="text-white">No data</div>;
 
   return (
     <div>
@@ -42,8 +51,8 @@ const Homepage: React.FC = () => {
       </svg>
       <div>
         <h1 className="text-2xl font-bold mb-4 text-center">Characters</h1>
-        <SearchBar />
-        <CharacterList characters={data.results} />
+        <SearchBar onSearch={handleSearch} />
+        <CharacterList characters={filteredCharacters} />
       </div>
     </div>
   );
